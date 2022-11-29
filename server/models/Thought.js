204147c2425592e5b8661c4ed22fb4bebc61b373
jsong73 <jessicajinsong@gmail.com>
2022-11-29 00:01:34 -0500

    created reaction model

---
+++ server/models/Thought.js
@@ -0,0 +1,32 @@
+const {Schema, model} = require("mongoose");
+const reactionSchema =  require("./Reaction");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String, 
+            required: "You need to leave a thought!",
+            maxlength: 280,
+            minlength: 1,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+        },
+        username:{
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+        },
+        {
+            toJSON:{
+                getters: true,
+            },
+            id: false
+        }
+);
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
