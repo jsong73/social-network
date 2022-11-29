d0ffdaf03c8b128c0ca86c2c5513cc324c54cc18
jsong73 <jessicajinsong@gmail.com>
2022-11-28 23:55:50 -0500

    settings up models

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model }= require("mongoose");
+
+const validateEmail = function(email){
+    const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return re.test(email)
+}
+
+const userSchema = new Schema(
+    {
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        validate: [validateEmail, "Please fill a valid email address!"]
+    },
+    thoughts:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought",
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    },
+    {
+        toJSON: {
+         // used for formatting and combining fields and de-composing a single value into multiple values before storing it in the collection.
+           virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model("User", userSchema)
+
+module.exports = User;
