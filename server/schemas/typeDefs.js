574bb5bc723d72d9379495392ed35fead52dea1b
jsong73 <jessicajinsong@gmail.com>
2022-11-29 23:37:55 -0500

    working on typedefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,35 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        thoughts: [Thought]!
+        friends: [User]!
+    }
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createAt: String
+        username: String
+        reactions: [Reaction]
+    }
+    type Reaction {
+        reactionId: ID
+        reactionBody: String
+        username: String
+        createdAt: String
+    }
+    type Query {
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(thoughtId: ID!): Thought
+        reactions: [Reaction]
+        reaction:(reactionId: ID!): Reaction
+        me: User
+    }
+
+`
--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const db = require("./config/connection")
+const { ApolloServer } = require("apollo-server-express")
+const { typeDefs, resolvers } = require("./schemas");
 
 const PORT = process.env.PORT || 3007;
-
 const app = express();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async(typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({ app });
+
+
 db.once("open", () => {
     app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
 })
-});
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers)
