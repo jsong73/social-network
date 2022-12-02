e2d6e42e9ab525bb43859eed71e295ba267fa826
jsong73 <jessicajinsong@gmail.com>
2022-12-02 14:29:56 -0500

    working on queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -12,9 +12,51 @@ export const QUERY_USER = gql`
                 createdAt
             }
             friends{
-                _id
+                username
             }
         }
     }
+`
 
+export const QUERY_THOUGHTS = gql`
+    query getThoughts  {
+        thoughts {
+            _id
+            thoughtText
+            username
+            createdAt
+        }
+    }
+`
+
+export const QUERY_SINGLE_THOUGHT = gql`
+    query getSingleThought($thoughtId: ID!) {
+        thought(thoughtId: $thoughtId) {
+            _id
+            thoughtText
+            thoughtAuthor
+            createdAt
+            reaction {
+                reactionId
+                reactionBody
+                username
+                createdAt
+        }
+    }
+}
+`
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            thoughts {
+                _id
+                thoughtText
+                username
+                createdAt
+            }
+        }
+    }
 `
