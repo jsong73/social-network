505d784e0b5a9e889a70732754f74e924b62fc67
Junghoon Yoon <jyoon3@2u.com>
2022-12-01 20:41:54 -0500

    pushing to resolve login issues

--- server/models/Thought.js
+++ server/models/Thought.js
@@ -1,31 +1,30 @@
-const {Schema, model} = require("mongoose");
-const reactionSchema =  require("./Reaction");
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String, 
-            required: "You need to leave a thought!",
-            maxlength: 280,
-            minlength: 1
-        },
-        createdAt:{
-            type: Date,
-            default: Date.now
-        },
-        username:{
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
-        },
-        {
-            toJSON:{
-                getters: true,
-            },
-            id: false,
-            versionKey: false
-        }
+  {
+    thoughtText: {
+      type: String,
+      required: "You need to leave a thought!",
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+    versionKey: false,
+  }
 );
 
 const Thought = model("Thought", thoughtSchema);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,48 +1,48 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        password: String
-        thoughts: [Thought]!
-        friends: [User]!
-    }
-    type Thought {
-        _id: ID
-        thoughtText: String
-        createdAt: String
-        username: String
-        reactions: [Reaction]
-    }
-    type Reaction {
-        reactionId: ID
-        reactionBody: String
-        username: String
-        createdAt: String
-    }
-    type Auth {
-        token: ID!
-        user: User
-      }
-    type Query {
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(thoughtId: ID!): Thought
-        reactions: [Reaction]
-        reaction(reactionId: ID!): Reaction
-        me: User
-    }
-    type Mutation {
-        addUser(username: String!, email: String!, password: String!): Auth
-        login(email: String!, password: String!): Auth
-        addThought(thoughtText: String!): Thought
-        addReaction(reactionId: ID!, reactionBody: String!): Reaction
-        removeThought(thoughtId: ID!): Thought
-        removeReaction(reactionId: ID!): Reaction
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    thoughts: [Thought]!
+    friends: [User]!
+  }
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactions: [Reaction]
+  }
+  type Reaction {
+    reactionId: ID
+    reactionBody: String
+    username: String
+    createdAt: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(thoughtId: ID!): Thought
+    reactions: [Reaction]
+    reaction(reactionId: ID!): Reaction
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(reactionId: ID!, reactionBody: String!): Reaction
+    removeThought(thoughtId: ID!): Thought
+    removeReaction(reactionId: ID!): Reaction
+  }
 `;
 
 module.exports = typeDefs;
