8b75eeaa072064efe7a94c0222bd28ed25e09e95
jsong73 <jessicajinsong@gmail.com>
2022-12-02 00:08:07 -0500

    working on routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import React from "react"
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
+
 
 function App() {
   return (
     <div className="App">
-HELLO
+    HELLO
     </div>
   );
 }
