86600be755793b214ac6a7ef29a6dc69203d3261
jsong73 <jessicajinsong@gmail.com>
2022-12-02 13:30:54 -0500

    made page routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,39 @@
 import './App.css';
 import React from "react"
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client"
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from "@apollo/client"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+import Signup from "./pages/Signup"
+import SingleThought from "./pages/SingleThought"
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
 
 
+
 function App() {
   return (
-    <div className="App">
-    HELLO
+    <div className="container">
+ <Routes>
+    <Route path="/" element={<Home />} />
+
+    <Route path="/login" element={<Login />} />
+
+    <Route path="/signup" element={<Signup />} />
+
+    <Route path="/me" element={<Profile />} />
+
+    <Route path="/profile/:username" element={<Profile />} />
+
+    <Route path="thoughts/:thoughtId" element={<SingleThought />} />
+ 
+ </Routes>
     </div>
   );
 }
---
+++ client/src/pages/SingleThought.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SingleThought = () => {
+    return(
+        <div>
+            SingleThought
+        </div>
+
+    )
+}
+
+export default SingleThought;
