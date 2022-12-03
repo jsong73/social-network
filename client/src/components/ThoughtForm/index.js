581e2e29349bf6f3cf95fa8a97892cf9e0c40e84
jsong73 <jessicajinsong@gmail.com>
2022-12-02 19:10:03 -0500

    made a thought form

---
+++ client/src/components/ThoughtForm/index.js
@@ -0,0 +1,74 @@
+import React, {useState} from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_THOUGHT } from "../../utils/mutations"
+import  { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries"
+import Auth from "../../utils/auth";
+
+const ThoughtForm = () => {
+    const [thoughtText, setThoughtText ] = useState("");
+
+    const [charactherCount, setCharacterCount ] = useState(0);
+
+    const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+        update(cache, {data: {addThought}}){
+            try {
+                const {thoughts } = cache.readQuery({ query: QUERY_THOUGHTS});
+
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts]},
+                });
+            } catch (error){
+                console.log(error);
+            }
+
+            const { me } = cache.readQuery({ query: QUERY_ME});
+            cache.writeQuery({
+                query: QUERY_ME, 
+                data:{ me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
+        },
+    });
+
+    const thoughtFormHandler = async (event) => {
+        event.preventDefault();
+        try {
+            const { data } = await addThought ({
+                variables: {
+                    thoughtText,
+                    username: Auth.getProfile().data.username,
+                },
+            });
+
+            setThoughtText("");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleChange = (event) => {
+        const {name, value } = event.target;
+
+        if ( name === "thoughtText" && value.length <= 280) {
+            setThoughtText(value);
+            setCharacterCount(value.length);
+        }
+    };
+
+    return(
+    <div>
+        <h1> Hello {Auth.getProfile().data.username}, </h1>
+            <form onSubmit={thoughtFormHandler}>
+                <textarea
+                    name="thoughtText"
+                    placeholder="Whats on your mind?"
+                    value={thoughtText}
+                    onChange={handleChange}></textarea>
+            
+             <button type="submit"> Submit </button>
+             </form>
+    </div>
+    )
+}
+
+export default ThoughtForm;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 import Navbar from "../components/Navbar"
-
+import ThoughtForm from "../components/ThoughtForm"
 
 const Home = () => {
+
+    const {loading, data} = useQuery(QUERY_THOUGHTS)
+    // const thoughts = data?.thoughts || [];
 
     return (
       <main>
         <Navbar />
+        <div style={{border: "1px solid black"}}>
+            <ThoughtForm />
+        </div>
+        <div>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+          <div> </div>
+          )}
+          </div>
       </main>
     );
   };
