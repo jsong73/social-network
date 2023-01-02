8475de8dfc9204160269f0b557db16a352fbccfc
jsong73 <jessicajinsong@gmail.com>
2023-01-02 12:31:05 -0500

    fixing errors

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import {
 } from "@apollo/client";
 
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom" ;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
